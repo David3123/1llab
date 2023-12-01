48fd97b8a81b536dd9335ce9f505d25438f9eafa
David3123 <79217730303david@gmail.com>
2023-12-01 13:30:03 +0300

    first commit

---
+++ Graphs/app/src/main/java/manvelyan/david403/graphs/Link.java
@@ -0,0 +1,11 @@
+package manvelyan.david403.graphs;
+
+public class Link {
+    public int a, b;
+
+    public Link(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+}
---
+++ Graphs/app/src/main/java/manvelyan/david403/graphs/Node.java
@@ -0,0 +1,10 @@
+package manvelyan.david403.graphs;
+
+public class Node {
+    public float x, y;
+
+    public Node(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+}
